5c0d119046b9248fc171e016e2f87ee71f5ca341
Ian Snyder-Sohl <iansnyder98@gmail.com>
2023-08-06 14:55:54 -0500

    added updatePost in posts.js, added updatePostForm.hbs, and removed unnec hbs file

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,6 +1,7 @@
 const mysql = require('mysql2');
 const connection = require("../config/connection");
 
+// CREATE a new post w/ user input
 async function createPost(req, res, user) {
     try {
         const userID = user.id;
@@ -19,13 +20,45 @@ async function createPost(req, res, user) {
     }
 }
 
+// UPDATE a post
+async function updatePost(req, res, user, postId) {
+    try {
+        const userID = user.id;
+        const { title, body } = req.body;
+
+        // Check if the post being updated belongs to the logged-in user
+        const selectQuery = "SELECT * FROM posts WHERE id = ? AND user_id = ?";
+        const [postResults] = await connection.query(selectQuery, [postId, userID]);
+
+        // the middleware should handle this with the req.session.user.id value and that should be passed in as the userID parameter. If the length of the results array is 0, then the post doesn't exist or doesn't belong to the user as defined by userID, so show an error... whew.
+        if (postResults.length === 0) {
+            return res.status(403).send("You are not authorized to update this post.");
+        }
+
+        // Update the post in the database
+        const updateQuery = "UPDATE posts SET title = ?, body = ? WHERE id = ? AND user_id = ?";
+        const values = [title, body, postId, userID];
+
+        await connection.query(updateQuery, values);
+        console.log("Post updated with ID: " + postId);
+
+        res.redirect("/");
+
+    } catch (err) {
+        console.log("Error updating post: " + err);
+        res.status(500).send("Error updating post");
+    }
+}
+
+// render the form for the post on the "create-post" view
 renderCreatePostForm = (req, res) => {
     res.render("create-post");
 };
 
 module.exports = {
     createPost,
-    renderCreatePostForm
+    renderCreatePostForm,
+    updatePost
 };
 
 
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -20,16 +20,17 @@ async function getAllPosts() {
 router.get("/", async (req, res) => {
   try {
     const isLoggedIn = req.session.isLoggedIn || false;
-
     const username = isLoggedIn && req.session.user ? req.session.user.username : '';
+    const userID = isLoggedIn && req.session.user ? req.session.user.id : null;
 
-    const posts = await getAllPosts();
+    const query = "SELECT posts.*, users.username, (posts.user_id = ?) AS belongsToCurrentUser FROM posts JOIN users ON posts.user_id = users.id";
+    const [results] = await connection.query(query, [userID]);
+
     res.render("index", {
       isLoggedIn,
       username,
-      posts,
+      posts: results, // Pass the posts data (including belongsToCurrentUser) to the "index" template
     });
-
   } catch (err) {
     res.status(500).send("Error fetching posts");
   }
@@ -52,18 +53,20 @@ router.get("/private", checkAuth, ({ session: { isLoggedIn } }, res) => {
 });
 
 
-// render the form for the post
+// Render the form for the post
 router.get("/create-post", posts.renderCreatePostForm);
 
 // "forum-content" view
 router.get("/forum-content", async (req, res) => {
   try {
-    // Query the database to get all posts
-    const query = "SELECT posts.*, users.username FROM posts JOIN users ON posts.user_id = users.id";
-    const [results] = await connection.query(query);
+    const userID = req.session.isLoggedIn ? req.session.user.id : null;
+
+    // Query the database to get all posts with an additional column indicating if it belongs to the current user
+    const query = "SELECT posts.*, users.username, (posts.user_id = ?) AS belongsToCurrentUser FROM posts JOIN users ON posts.user_id = users.id";
+    const [results] = await connection.query(query, [userID]);
 
     // Pass the posts data to the "forum-content" template
-    res.render("loggedIn", { posts: results });
+    res.render("/", { posts: results });
   } catch (err) {
     res.status(500).send("Error fetching posts");
   }
@@ -73,6 +76,36 @@ router.get("/forum-content", async (req, res) => {
 router.post("/create", checkAuth, (req, res) => {
   console.log("req.session.user:", req.session.user)
   posts.createPost(req, res, req.session.user);
+});
+
+// Handle the update request for a specific post
+router.get("/update/:postId", checkAuth, async (req, res) => {
+  try {
+    const postId = req.params.postId;
+    const userID = req.session.user.id;
+
+    // Query the database to get the post with the given postId
+    const selectQuery = "SELECT * FROM posts WHERE id = ? AND user_id = ?";
+    const [postResults] = await connection.query(selectQuery, [postId, userID]);
+
+    // If the post doesn't exist or doesn't belong to the user, show an error or redirect
+    if (postResults.length === 0) {
+      // You can show an error message or redirect to a different page, for example:
+      return res.status(403).send("You are not authorized to update this post.");
+    }
+
+    // Render the update-post form passing the existing post data
+    res.render("updatePostForm", { post: postResults[0] });
+
+  } catch (err) {
+    res.status(500).send("Error fetching post for update");
+  }
+});
+
+// POST route to handle the actual update of the post
+router.post("/update/:postId", checkAuth, (req, res) => {
+  const postId = req.params.postId;
+  posts.updatePost(req, res, req.session.user, postId);
 });
 
 module.exports = router;
